e1d7bb8d6c61a372b53ae2314690c8d6915670b5
Laghrabi <laghrabichaimae@gmail.com>
2026-07-23 17:59:52 +0100

    constructor

--- srcs/HTTP/request/HttpRequest.cpp
+++ srcs/HTTP/request/HttpRequest.cpp
@@ -37,7 +37,9 @@ HttpRequest::HttpRequest() : _statusCode(OK), _currentState(READING_REQUEST_LINE
 		_client_max_body_size(other._client_max_body_size),
 		_server(other._server),
 		_serverRange(other._serverRange),
-		_clientEndPoint(other._clientEndPoint) {}
+		_clientEndPoint(other._clientEndPoint),
+		_bodyFilePath(other._bodyFilePath),
+		_bodyStream(other._bodyStream) {}
 
 		/**
 		 * @brief Copy assignment operator for HttpRequest.
@@ -67,6 +69,7 @@ HttpRequest::HttpRequest() : _statusCode(OK), _currentState(READING_REQUEST_LINE
 				_server = other._server;
 				_serverRange = other._serverRange;
 				_clientEndPoint = other._clientEndPoint;
+				_bodyFilePath = other._bodyFilePath;
 			}
 			return (*this);
 		}
